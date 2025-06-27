46a199c75914394ca7ce8d2e8ea1a67d92f3adeb
EvertnRS <evertosilves@gmail.com>
2025-06-26 21:03:41 -0300

    feat: add CORS support to the server and update dependencies

--- server.ts
+++ server.ts
@@ -1,35 +1,42 @@
-import express  from "express";
+import express from "express";
+import cors from "cors";
 import { Server } from "socket.io";
 import http from "http";
 import userRoutes from "./src/routes/userRoutes";
 import chatRoutes from "./src/routes/chatRoutes";
 import messageRoutes from "./src/routes/messageRoutes";
 import { WebSocketProvider } from "./src/infra/providers/websocket/WebSocketProvider";
-import swaggerUi from 'swagger-ui-express';
-import { swaggerDocument } from './src/infra/swagger/swagger';
+import swaggerUi from "swagger-ui-express";
+import { swaggerDocument } from "./src/infra/swagger/swagger";
 
 const app = express();
-const httpServer = http.createServer(app);
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*"
-  },
-});
 
-const webSocketProvider = WebSocketProvider.startInstance(io);
-webSocketProvider.setupSocket(io);
+app.use(cors({
+  origin: "*",
+  credentials: true,
+}));
 
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const httpServer = http.createServer(app);
 
-app.use('/', userRoutes);
-app.use('/', chatRoutes);
-app.use('/', messageRoutes);
-
-httpServer.listen(3000, '0.0.0.0', () => {
-  console.log('Server rodando na porta 3000');
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
 });
 
+const webSocketProvider = WebSocketProvider.getInstance();
+webSocketProvider.setupSocket(io);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/", userRoutes);
+app.use("/", chatRoutes);
+app.use("/", messageRoutes);
+
+httpServer.listen(3000, () => {
+  console.log("Server ta rodandinho na portinha http://localhost:3000");
+});
 
 export { io };
