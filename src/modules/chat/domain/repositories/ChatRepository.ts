56379db3da17d0e348e1c01b29b0b126f4dc012e
EvertnRS <evertosilves@gmail.com>
2025-06-27 21:30:32 -0300

    fix: remove insensitive mode from search query in findByName method

--- src/modules/chat/domain/repositories/ChatRepository.ts
+++ src/modules/chat/domain/repositories/ChatRepository.ts
@@ -72,7 +72,6 @@ export class ChatRepository implements IChatRepository {
       if (search) {
         query.name = {
           contains: search
-          // mode: 'insensitive'
         };
       }
 
