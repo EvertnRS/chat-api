56e784826c205fb63db5a84092793c8a88adb6f3
kio <kaio.viana@upe.br>
2025-07-03 18:26:40 -0300

    feat: add user name and email to login response

--- src/@types/user/LoginResponse.ts
+++ src/@types/user/LoginResponse.ts
@@ -6,6 +6,8 @@ extendZodWithOpenApi(z);
 export const LoginResponseSchema = z.object({
     user: z.object({
         id: z.string().uuid().openapi({ example: '123e4567-e89b-12d3-a456-42661417400' }),
+        name: z.string().min(1).openapi({ example: 'John Doe' }),
+        email: z.string().email().openapi({ example: 'john@example.com'})
     }),
     token: z.string().openapi({ example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...' }),
 });
